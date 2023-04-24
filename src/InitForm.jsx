e4af37e9e9915c6c0ed87bee9ab9dff523e6d7c7
eViewer HTML5 Document Viewer <viewer@eviewer.net>
2023-04-24 13:03:58 -0700

    Updated to v7.0.253

--- src/InitForm.jsx
+++ src/InitForm.jsx
@@ -1,7 +1,7 @@
 import React, { Component, useState } from "react";
 import App from "./App";
 import ReactDOM from "react-dom";
-import eViewerApp from "eviewerjs/js/eViewer7.js";
+import eViewerApp from "@mstechusa/eviewer7/js/eViewer7.js";
 import Form from "./form";
 
 class InitForm extends Component {
@@ -11,8 +11,7 @@ class InitForm extends Component {
       showForm: false,
       isChecked: true,
       userName: "demo user",
-      licenseKey:
-        "QX0ZUhau5qt0HBLnDQ5XcL+Ih625vvsKCk3fjacNqa/3+F6sDkQFUkso2z6Fi9z/w7MhHwhp0K7q0eX/agSjfY/q9HOBHnr7f4TY81KZosgmOKn0PnHHc0te9+Ps4anZ8G0Ub1lsIO2vL6CNpB5yjHeWY1E+0M7GFQYdtXowd4brMwfESdAvLEPibII77mwVmNvGzyEzNUpX1Krknne2vJzI8+jKs93zu0ADdxPUz1aPqeTTVhwokyPx6aE5aMQJA7/vTBJiIha4h7YX3oWjhspTeBObRRSMIvH/iKYqZJeMIA3pNj+7WD9W1cqnuvvScErUKbzOzGCD/Za9y9GPrRY3HcpxWoyhaKivjdRdUOAn/lx2onD8VIxwKoPJJgck16IHK2kqmwpz2CEk3LbM/hpW2bLWaQ1eaqA31gb5yWneUiIpnHKvLCnf+xrQk/235nMbpmsCyh6X5DAs4w2n7RzrHME67rpu7rIBOL/LZj4=",
+      licenseKey: "PROVIDED BY MST",
       viewerServerURL: "",
     };
   }
